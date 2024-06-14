7acde78e3e96de10af50e37d52b634f11507d6d5
vedharagavan2404 <vrpadagalingam@myseneca.ca>
2024-06-14 01:17:04 -0400

    added code deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world, code deployment is done!'
+    return 'Hello, world, code deployment is completed!'
 
 if __name__ == '__main__':
     app.run()
