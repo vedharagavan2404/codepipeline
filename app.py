13c8d369825979cfd91f49bdc862297296587bd1
vedharagavan2404 <vrpadagalingam@myseneca.ca>
2024-06-15 03:13:59 -0400

    final commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World, code deployments is completed!'
+    return 'Hello World, code deployment is finally completed!'
 
 if __name__ == '__main__':
     app.run()
